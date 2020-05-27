98514318d507b0e164adb0de483b7a730e1bf762
Shriya235 <65900306+Shriya235@users.noreply.github.com>
2020-05-27 12:26:54 +0530

    Add files via upload

---
+++ Uptime_new/config.py
@@ -0,0 +1,3 @@
+logfile='alpha_board_router_stats.txt'
+
+interface_list=['ath0','ath1','ath2']
---
+++ Uptime_new/main_prog.py
@@ -0,0 +1,5 @@
+import config as conf
+import Uptime as cf
+
+
+cf.Call_uptimelist(conf.interface_list,conf.logfile) 
